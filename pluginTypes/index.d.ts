96ac6b3ca2fb8ff8164233d5ab433c697add6708
YC Wong <66290913+yc-wong@users.noreply.github.com>
2024-12-05 21:17:48 +0800

    update translations

--- pluginTypes/index.d.ts
+++ pluginTypes/index.d.ts
@@ -24,29 +24,40 @@ declare module "@scom/scom-tx-status-modal/index.css.ts" {
 declare module "@scom/scom-tx-status-modal/translations.json.ts" {
     const _default_2: {
         en: {
-            waiting_for_confirmation: string;
+            cancel: string;
+            close: string;
             confirm_this_transaction_in_your_wallet: string;
             loading: string;
-            view_on_block_explorer: string;
-            transaction_submitted: string;
-            close: string;
-            cancel: string;
             not_enough_gas_to_process_transaction: string;
-            unknow_error: string;
             transaction_rejected: string;
+            transaction_submitted: string;
+            unknow_error: string;
+            view_on_block_explorer: string;
+            waiting_for_confirmation: string;
         };
-        "zh-hant": {};
-        vi: {
-            waiting_for_confirmation: string;
+        "zh-hant": {
+            cancel: string;
+            close: string;
             confirm_this_transaction_in_your_wallet: string;
             loading: string;
-            view_on_block_explorer: string;
-            transaction_submitted: string;
-            close: string;
-            cancel: string;
             not_enough_gas_to_process_transaction: string;
-            unknow_error: string;
             transaction_rejected: string;
+            transaction_submitted: string;
+            unknow_error: string;
+            view_on_block_explorer: string;
+            waiting_for_confirmation: string;
+        };
+        vi: {
+            cancel: string;
+            close: string;
+            confirm_this_transaction_in_your_wallet: string;
+            loading: string;
+            not_enough_gas_to_process_transaction: string;
+            transaction_rejected: string;
+            transaction_submitted: string;
+            unknow_error: string;
+            view_on_block_explorer: string;
+            waiting_for_confirmation: string;
         };
     };
     export default _default_2;
